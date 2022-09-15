9449bd46e51614e0793a3f9d75c99b89d88e3f27
ayoshi007 <aki.f.yoshimoto@gmail.com>
2022-09-15 17:08:36 -0500

    Moved gradient descent functions to separate file

--- linreg_model.py
+++ linreg_model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+from vanillagd import gradient_descent, mse_gradient
 from sklearn.metrics import mean_squared_error, r2_score
 
 class LinRegModel:
@@ -14,7 +15,7 @@ class LinRegModel:
         n = np.size(X_train, axis=1) + 1
         data_pts = self.__append_ones(X_train)
         weights = np.random.randn(1, n)
-        self.weights = mse_gradient(weights, data_pts, y_train)
+        self.weights = gradient_descent(mse_gradient, data_pts, y_train, weights, self.learn_rate, self.n_iter, self.tolerance)
 
     def predict(self, X_test):
         self.__check_fit()
@@ -47,28 +48,3 @@ class LinRegModel:
         if not self.check_fit():
             raise Exception("Model has not been fit")
 
-
-def gradient_descent(gradient, features, targets, start_weights, learn_rate=1, n_iter=1000, tolerance=0.001):
-    weights = start_weights
-    for i in range(n_iter):
-        negative_grad = -learn_rate * np.array(gradient(features, targets, weights))
-        if np.all(np.abs(negative_grad) <= tolerance):
-            break
-        weights += negative_grad
-    return weights
-
-
-'''
-mse = (1/2m)sum(y - ^y)^2 - sum is across all data points
-
-mse_gradient for feature i is: (1/m)sum(y - ^y) * xi
-    xi = feature i for data point
-'''
-'''
-need to check/test
-'''
-def mse_gradient(data_pts, targets, weights):
-    # get the residuals for each data point prediction for each data point with current weights
-    res = np.array([np.dot(i, weights) for i in data_pts]) - targets
-    # get the gradient by taking the mean of the sum between each data point column and their residual
-    return [(column * res).mean() for column in data_pts.transpose()]
