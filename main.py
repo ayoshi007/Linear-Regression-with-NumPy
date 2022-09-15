264656b5daacc452db1ead2f0353286b47792368
ayoshi007 <aki.f.yoshimoto@gmail.com>
2022-09-15 17:40:41 -0500

    Cleaned up main

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from dataset import PortugueseStudentMathGradesDataSet
 from linreg_model import LinRegModel
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-import numpy as np
 
 dataset = PortugueseStudentMathGradesDataSet('student-mat.csv', 'https://personal.utdallas.edu/~afy180000/intro_ml/assignment1/')
 
@@ -12,6 +9,7 @@ X_train, X_test, y_train, y_test = dataset.get_split()
 learn_rate = 0.0000001
 n_iter = 10000
 tolerance = 1e-06
+
 model = LinRegModel(learn_rate=learn_rate, n_iter=n_iter, tolerance=tolerance)
 model.fit(X_train, y_train)
 preds = model.predict(X_test)
