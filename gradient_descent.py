f402f50ed94a14bb5460810b17cfd26502c376f1
ayoshi007 <aki.f.yoshimoto@gmail.com>
2022-09-24 21:47:16 -0500

    Added stochastic gradient descent

---
+++ gradient_descent.py
@@ -0,0 +1,40 @@
+import numpy as np
+import random
+
+def gradient_descent(gradient, features, targets, start_weights, learn_rate=1, n_iter=1000, tolerance=0.001):
+    weights = start_weights
+    for i in range(n_iter):
+        # get gradient 'step'
+        negative_grad = -learn_rate * np.array(gradient(features, targets, weights))
+        # terminate if below tolerance
+        if np.all(np.abs(negative_grad) <= tolerance):
+            break
+        # make gradient 'step'
+        weights += negative_grad
+    # return fitted weights
+    return weights
+
+
+def mse_gradient(data_pts, targets, weights) -> list:
+    # get the residuals for each data point prediction for each data point with current weights
+    res = np.array([np.dot(i, weights) for i in data_pts]) - targets
+    # get the gradient by taking the mean of the sum between each data point column and their residual
+    return [(column * res).mean() for column in data_pts.transpose()]
+
+
+def sgd(gradient, features, targets, start_weights, learn_rate=1, n_iter=1000, tolerance=0.001, mini_batch=1):
+    data = np.insert(features, features.shape[1], targets, axis=1)
+    np.random.shuffle(data)
+    features = data[:, :-1]
+    targets = data[:, -1]
+    weights = start_weights
+    for _ in range(n_iter):
+        i = random.randint(0, targets.size - mini_batch)
+        batch_features = features[i:mini_batch + i]
+        batch_targets = targets[i:mini_batch + i]
+        negative_grad = -learn_rate * np.array(gradient(batch_features, batch_targets, weights))
+        if np.all(np.abs(negative_grad) <= tolerance):
+            break
+        weights += negative_grad
+    # return fitted weights
+    return weights
--- linreg_model.py
+++ linreg_model.py
@@ -1,20 +1,5 @@
 import numpy as np
-
-def gradient_descent(gradient, features: np.ndarray, targets: np.ndarray, start_weights: np.ndarray, learn_rate=1, n_iter=1000, tolerance=0.001):
-    weights = start_weights
-    for i in range(n_iter):
-        negative_grad = -learn_rate * np.array(gradient(features, targets, weights))
-        if np.all(np.abs(negative_grad) <= tolerance):
-            break
-        weights += negative_grad
-    return weights
-
-
-def mse_gradient(data_pts: np.ndarray, targets: np.ndarray, weights: np.ndarray) -> list:
-    # get the residuals for each data point prediction for each data point with current weights
-    res = np.array([np.dot(i, weights) for i in data_pts]) - targets
-    # get the gradient by taking the mean of the sum between each data point column and their residual
-    return [(column * res).mean() for column in data_pts.transpose()]
+from gradient_descent import gradient_descent, mse_gradient, sgd
 
 class SelfCodedLinRegModel:
     def __init__(self, params: dict):
